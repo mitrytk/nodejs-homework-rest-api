546c16607504d21a63f48b15e032e10421f2f04f
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-05-24 00:02:35 +0200

    connect to mangoodb

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const app = require("./app");
+const mongoost = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000");
-});
+const app = require("./app");
+const DB_HOST =
+  "mongodb+srv://Dmitry:QSyYUNNKd%402E9qg@cluster0.x8vtve9.mongodb.net/contacts_reader";
+
+mongoost
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// QSyYUNNKd@2E9qg
