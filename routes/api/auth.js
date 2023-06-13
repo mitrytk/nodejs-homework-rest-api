ad54a2bafdce8ff063854dc0b726dcde5cda7202
Дмитрий Ткаченко <dmitrytkachenko7397@gmail.com>
2023-06-13 16:19:13 +0200

    add verify via email

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const { volidateBody, authenticate, upload } = require("../../middlewares");
-const { registerSchema, loginSchema } = require("../../schemas");
+const { registerSchema, loginSchema, verifySchema } = require("../../schemas");
 const ctrl = require("../../controllers/auth");
 
 router.post("/register", volidateBody(registerSchema), ctrl.register);
+router.get("/verify/:verificationToken", ctrl.verifyEmail);
+router.post("/verify", volidateBody(verifySchema), ctrl.resendVerifyEmail);
 router.post("/login", volidateBody(loginSchema), ctrl.login);
 router.get("/current", authenticate, ctrl.getCurrent);
 router.post("/logout", authenticate, ctrl.logout);
